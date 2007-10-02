0a6efae8d8ed47bf746ff5c13e0bb9bb45c12e37
fvitale <fvitale@aa42c9ba-4e3b-0410-b116-b3c2c13edfc6>
2007-10-02 20:51:26 +0000

    housekeeping

--- fineco2qif/src/test/java/it/filippovitale/fineco2qif/test/MainTest.java
+++ fineco2qif/src/test/java/it/filippovitale/fineco2qif/test/MainTest.java
@@ -4,51 +4,23 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class MainTest {
-	@BeforeClass public static void systemSetup() {
-		// run once
-	}
-
-	@Before public void beforeEveryTest() {
-		// run once
-	}
-
-	@After public void afterEveryTest() {
-		// run once
-	}
 
 	@Test
-	public void prova() {
+	public void assertThatTest() {
 		assertThat(true, is(true));
 		assert true;
 		assertThat(false, is(not(true)));
 		assert !true;
 	}
 
-
-	@Ignore("not now, please") 
-	@Test
-    public void divideByZeroNotNow() {
-        int a = 1/0;
-        a++;
+	@Test(timeout=20, expected=RuntimeException.class)
+	@Ignore("not implemented yet") 
+    public void importCreditcardSample1() {
+		throw new RuntimeException("not implemented yet"); 
     }
-
-	@Test(timeout=20, expected=ArithmeticException.class)
-    public void divideByZero() {
-		// it should take less than 20ms
-        int a = 1/0;
-        a++;
-    }
-
-	@AfterClass public static void systemShutdown() {
-		// run once
-	}
 
 }
